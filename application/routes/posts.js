6ec3f9ca2d3abbdf35144aa0f2c993f8418fa169
Ze Lei <zlei1@mail.sfsu.edu>
2020-12-18 13:21:48 -0800

    added front and back end for searching for posts

--- application/routes/posts.js
+++ application/routes/posts.js
@@ -27,8 +27,8 @@ router.post('/createPost', uploader.single("uploadImage"), (req, res, next) => {
     let desc = req.body.description;
 
     check(title).notEmpty(),
-    check(desc).notEmpty(),
-    check(fileUploaded).notEmpty()
+        check(desc).notEmpty(),
+        check(fileUploaded).notEmpty()
 
     var errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -40,31 +40,67 @@ router.post('/createPost', uploader.single("uploadImage"), (req, res, next) => {
         let fk_userId = req.session.userId;
 
         sharp(fileUploaded)
-        .resize(200)
-        .toFile(destinationOfThumbnail)
-        .then(() => {
-            let baseSQL = 'INSERT INTO posts (title, description, photopath, thumbnail, created, fk_userid) VALUE (?,?,?,?, now(), ?);';
-            return db.execute(baseSQL,[title, desc, fileUploaded, destinationOfThumbnail, fk_userId]);
-        })
-        .then(([results, fields]) => {
-            if(results && results.affectedRows){
-                req.flash('success', 'your post was created successfully');
-                res.redirect('/');
-            }else{
-                throw new PostError('post could not be created', '/postimage', 200);
-            }
-        })
-        .catch((err) => {
-            if(err instanceof PostError){
-                errorPrint(err.getMessage());
-                req.flash('error', err.getMessage());
-                res.status(err.getStatus());
-                res.redirect(err.getRedirectURL());
-            }else{
-                next(err);
-            }
-        })
+            .resize(200)
+            .toFile(destinationOfThumbnail)
+            .then(() => {
+                let baseSQL = 'INSERT INTO posts (title, description, photopath, thumbnail, created, fk_userid) VALUE (?,?,?,?, now(), ?);';
+                return db.execute(baseSQL, [title, desc, fileUploaded, destinationOfThumbnail, fk_userId]);
+            })
+            .then(([results, fields]) => {
+                if (results && results.affectedRows) {
+                    req.flash('success', 'your post was created successfully');
+                    res.redirect('/');
+                } else {
+                    throw new PostError('post could not be created', '/postimage', 200);
+                }
+            })
+            .catch((err) => {
+                if (err instanceof PostError) {
+                    errorPrint(err.getMessage());
+                    req.flash('error', err.getMessage());
+                    res.status(err.getStatus());
+                    res.redirect(err.getRedirectURL());
+                } else {
+                    next(err);
+                }
+            })
     }
 });
+
+router.get('/search', (req, res, next) => {
+    let searchTerm = req.query.search;
+    if (!searchTerm) {
+        res.send({
+            resultsStatus: "info",
+            message: "no search term given",
+            results: []
+        });
+    } else {
+        let baseSQL = "SELECT id, title, description, thumbnail, concat_ws(' ', title, description) AS haystack \
+        FROm posts \
+        HAVING haystack like ?;";
+        let sqlReadySearchTerm = "%" + searchTerm + "%";
+        db.execute(baseSQL, [sqlReadySearchTerm])
+            .then(([results, fields]) => {
+                if (results && results.length) {
+                    res.send({
+                        resultsStatus: "info",
+                        message: `${results.length} results found`,
+                        results: results
+                    });
+                } else {
+                    db.query('SELECT id, title, description, thumbnail, created FROM posts ORDER BY created DESC LIMIT 8', [])
+                        .then(([results, fields]) => {
+                            res.send({
+                                resultsStatus: "info",
+                                message: 'No results found. 8 most recent posts displayed instead',
+                                results: results
+                            });
+                        })
+                }
+            })
+            .catch((err) => next(err));
+    }
+})
 
 module.exports = router;
