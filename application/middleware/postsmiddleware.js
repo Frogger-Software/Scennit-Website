4ea056c0b71f1edcdbb60ad79a0c2fe6f8820cc0
Ze Lei <zlei1@mail.sfsu.edu>
2020-12-18 17:34:08 -0800

    started on comments

--- application/middleware/postsmiddleware.js
+++ application/middleware/postsmiddleware.js
@@ -1,14 +1,30 @@
-var PostModel = require('../models/Posts');
+var {getNRecentPosts, getPostById} = require('../models/Posts');
 const postMiddleware = {};
 
 postMiddleware.getRecentPosts = async function(req, res, next){
     try{
-        let results = await PostModel.getNRecentPosts(8);
+        let results = await getNRecentPosts(8);
         res.locals.results = results;
         if(results.length == 0){
             req.flash('error', 'there are no posts created yet');
         }
         next();
+    }catch(err){
+        next(err);
+    }
+}
+
+postMiddleware.getPostById = async function(req, res, next){
+    try{
+        let postId = req.params.id;
+        let results = await getPostById(postId);
+        if(results && results.length){
+            res.locals.currentPost = results[0];
+            next();
+        }else{
+            res.flash('error', 'this is not the post you are looking for');
+            res.redirect('/');
+        }
     }catch(err){
         next(err);
     }
--- application/models/Posts.js
+++ application/models/Posts.js
@@ -5,10 +5,10 @@ PostModel.create = (title, description, photopath, thumbnail,
     fk_userId) => {
     let baseSQL = 'INSERT INTO posts (title, description, photopath, thumbnail, created, fk_userid) VALUE (?,?,?,?, now(), ?);';
     return db.execute(baseSQL, [title, description, photopath, thumbnail, fk_userId])
-    .then(([results, fields]) => {
-        return Promise.resolve(results && results.affectedRows);
-    })
-    .catch((err) => Promise.reject(err));
+        .then(([results, fields]) => {
+            return Promise.resolve(results && results.affectedRows);
+        })
+        .catch((err) => Promise.reject(err));
 }
 
 PostModel.search = (searchTerm) => {
@@ -17,15 +17,15 @@ PostModel.search = (searchTerm) => {
     HAVING haystack like ?;";
     let sqlReadySearchTerm = "%" + searchTerm + "%";
     return db.execute(baseSQL, [sqlReadySearchTerm])
-    .then(([results, fields]) => {
-        return Promise.resolve(results);
-    })
-    .catch((err) => Promise.reject(err));
+        .then(([results, fields]) => {
+            return Promise.resolve(results);
+        })
+        .catch((err) => Promise.reject(err));
 }
 
 PostModel.getNRecentPosts = (numberOfPosts) => {
-    let baseSQL = `SELECT id, title, description, thumbnail, \
-    created FROM posts ORDER BY created \
+    let baseSQL = `SELECT id, title, description, thumbnail,
+    created FROM posts ORDER BY created
     DESC LIMIT ${numberOfPosts}`;
     return db.execute(baseSQL)
         .then(([results, fields]) => {
@@ -33,5 +33,19 @@ PostModel.getNRecentPosts = (numberOfPosts) => {
         })
         .catch((err) => Promise.reject(err));
 };
+
+PostModel.getPostById = (postId) => {
+    let baseSQL = `SELECT u.username, p.title, p.description, p.photopath, p.created
+    FROM users u
+    JOIN posts p
+    ON u.id=fk_userid
+    WHERE p.id=?;`;
+
+    return db.execute(baseSQL, [postId])
+        .then(([results, fields]) => {
+            return Promise.resolve(results);
+        })
+        .catch(err => Promise.reject(err));
+}
 
 module.exports = PostModel;
