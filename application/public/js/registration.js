1cabab38792670633571cfe1d538d1069ef20433
Ze Lei <zlei1@mail.sfsu.edu>
2020-12-19 19:32:44 -0800

    spruced up frontend js for registration

--- application/public/js/registration.js
+++ application/public/js/registration.js
@@ -1,3 +1,5 @@
+var requirements = [false, false, false, false, false, false];
+
 function alphanumericCheck(str) {
     ascii = str.charCodeAt(0);
     if (ascii < 65 || ascii > 122) {
@@ -8,33 +10,86 @@ function alphanumericCheck(str) {
 
 let username = document.getElementById('username');
 username.onchange = function () {
-    var str = username.value;
-    if (!alphanumericCheck(str)) {
-        alert("Username must start with a character ([a-zA-Z])");
+    let req1 = document.getElementById('username-req1');
+    let req2 = document.getElementById('username-req2');
+    let str = username.value;
+    if (alphanumericCheck(str)) {
+        req1.style = "color: rgb(60, 255, 0)";
+        requirements[0] = true;
+    }else{
+        req1.style = "color: red";
+        requirements[0] = false;
     }
-    if (str.length < 3) {
-        alert("Username must be 3 or more alphanumeric characters");
+
+    if (str.length >= 3) {
+        req2.style = "color: rgb(60, 255, 0)";
+        requirements[1] = true;
+    }else{
+        req2.style = "color: red";
+        requirements[1] = false;
     }
+}
+
+let email = document.getElementById('email');
+email.onchange = function () {
+    let str = email.value;
+    let req = document.getElementById('email-req');
+    if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(str)){
+        req.style = "color: rgb(60, 255, 0)";
+        requirements[2] = true;
+    }else{
+        req.style = "color: red)";
+        requirements[2] = false;
+    }
+
 }
 
 let password = document.getElementById('password');
 password.onchange = function () {
-    var str = password.value;
+    let req1 = document.getElementById('pass-req1');
+    let req2 = document.getElementById('pass-req2');
+    let str = password.value;
     const passRegex = new RegExp("^(?=.*[A-Z])(?=.*[0-9])(?=.*[/\*-+!@#\$%\^&])");
-    if (str.length < 8) {
-        alert("Password must be 8 or more characters");
+    if (str.length >= 8) {
+        req1.style = "color: rgb(60, 255, 0)";
+        requirements[3] = true;
+    }else{
+        req1.style = "color: red";
+        requirements[3] = false;
     }
-    if (!passRegex.test(str)) {
-        alert("Password must contain at least 1 upper case letter and 1 number and 1 of the following special characters ( / * - + ! @ # $ ^ & * ).");
+
+    if (passRegex.test(str)) {
+        req2.style = "color: rgb(60, 255, 0)";
+        requirements[4] = true;
+    }else{
+        req2.style = "color: red";
+        requirements[4] = false;
     }
 }
 
 let passwordConfirm = document.getElementById('password-confirm');
 passwordConfirm.onchange = function () {
-    var str1 = password.value;
-    var str2 = passwordConfirm.value;
-    if (str1 != str2) {
-        alert("Passwords don't match");
+    let req = document.getElementById('cpass-req');
+    let str1 = password.value;
+    let str2 = passwordConfirm.value;
+    if (str1 = str2) {
+        req.style = "color: rgb(60, 255, 0)";
+        requirements[5] = true;
+    }else{
+        req.style = "color: red";
+        requirements[5] = false;
     }
 }
 
+function isTrue(bool){
+    return bool;
+}
+
+let registerButton = document.getElementById('register-button');
+registerButton.onclick = function () {
+    if(requirements.every(isTrue)){
+        return true;
+    }else{
+        return false;
+    }
+}
